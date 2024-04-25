e317d79e6c631a4173403a6e5e01c0ee2edc7579
Dmitriy <prshndmtr@yandex.ru>
2024-04-26 02:07:56 +1000

    transfer promts ftom Gigachat.py to settings

--- Gigachat.py
+++ Gigachat.py
@@ -1,12 +1,20 @@
-import logging.config
+from src.setting import (
+    PROMPTS_BASE_SYS,
+    PROMPT_FOR_ROADMAP,
+    PROMPT_FOR_QUASTION,
+    PROMPT_BASE,
+    PROMPT_FOR_GANT,
+)
 
 import logging
+import logging.config
 
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
 
-from db import get_gigachat_cred, get_user_id, insert_user, insert_user_task
+from db import get_gigachat_cred, insert_user_task
 from processing_data_for_db import processing_for_add_in_db
+
 
 logging.getLogger(__name__)
 credentials = get_gigachat_cred()
@@ -15,39 +23,18 @@ chat = GigaChat(
     credentials=credentials, verify_ssl_certs=False, scope="GIGACHAT_API_CORP"
 )
 
-prompts_base_sys = SystemMessage(
-    content=""" Ты ИИ-ассистент, который помогает строить план действий для решение задачи. 
-                             Если введенное сообщение похоже на запрос о создании плана, то напиши 'Да', иначе напиши 'Нет' """
-)
-
-prompt_for_quastion = SystemMessage(
-    """Ты умный студент, расскажи подробнее об этой теме и не забудь выделить самое важное"""
-)
-
-prompts_base = """Ты ИИ-ассистент, который помогает строить план действий для решения задачи. 
-                             Если введенное сообщение похоже на запрос о создании плана, то напиши 'Да', иначе напиши 'Нет'"""
-
-prompts_for_roadmap = SystemMessage(
-    content="""Представь, что ты умный студент. Разбей изучение введенной темы на уникальные пункты.
-    Важное условие: Пункты не могут повторяться.
-    Напиши каждый пункт и срок его выполнения в отдельной строке.
-    Срок указывай только по образцу ДД.ММ.
-    Сложи время выполнения всех пунктов и выведи в отдельную строку"""
-    # "Ты таск-менеджер, который получает информацию о сроках выполнения задачи, получает ее описание и дает ей название\n"
-)
-
 messages = {}
 
 
 def message_processing(tg_id: int, message: str) -> str:
 
-    quastion = prompts_base + "\n" + message
+    quastion = PROMPT_BASE + "\n" + message
 
     try:
         res = chat([HumanMessage(content=quastion)]).content
 
         if res.lower() == "да":
-            plan = chat([prompts_for_roadmap, HumanMessage(content=message)]).content
+            plan = chat([PROMPT_FOR_ROADMAP, HumanMessage(content=message)]).content
             # try:
             #     send = tg.bot.send_message(
             #         tg_id, "В какие сроки вы планируете это сделать?"
@@ -64,14 +51,20 @@ def message_processing(tg_id: int, message: str) -> str:
             for el in list_tasks_for_db:
                 insert_user_task(tg_id, el)
             return plan
+        
+        # res = chat([PROMPT_FOR_GANT, HumanMessage(content=message)]).content
+
+        # if res.lower() == "да":
+        #     way = Gant_Diagram(tg_id)
+        #     return way
         else:
             if tg_id in messages:
                 if len(messages[tg_id]) >= 10:
                     last_messages = messages[tg_id][2:]
-                    messages[tg_id] = [prompt_for_quastion] + last_messages
+                    messages[tg_id] = [PROMPT_FOR_QUASTION] + last_messages
                     messages[tg_id].append(HumanMessage(content=message))
             else:
-                messages[tg_id] = [prompt_for_quastion]
+                messages[tg_id] = [PROMPT_FOR_QUASTION]
                 messages[tg_id].append(HumanMessage(content=message))
             answer = chat(messages[tg_id]).content
             return answer
