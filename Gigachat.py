9b697c4be93eceb69d7527cd6ec4bc47349db92d
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-04-25 21:39:33 +1000

    refactor connect to database and add opportunity to insert tasks to database

--- Gigachat.py
+++ Gigachat.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 import logging
 import queue
@@ -6,47 +7,65 @@ import queue
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.chat_models.gigachat import GigaChat
 
-from db import connect, get_gigachat_cred, get_user_id, insert_user
+from db import connect, get_gigachat_cred, get_user_id, insert_user, insert_user_task
+from processing_data_for_db import processing_for_add_in_db
 
 
 logging.getLogger(__name__)
-db = connect()
-if not db:
-    sys.exit()
-credentials = get_gigachat_cred(db)
+credentials = get_gigachat_cred()
 
 chat = GigaChat(
     credentials=credentials, verify_ssl_certs=False, scope="GIGACHAT_API_CORP"
 )
 
-prompts_base = SystemMessage(
-    content="""Ты ассистент. Пользователи тебе отправляют сообщения. 
-            Тебе необходимо определить является сообщение задачей пользователя, вопросом к тебе или просьбой что-то сделать.
-            Ты должен определить является ли сообщение задачей или нет.
-            Ты должен отвечать на сообщение 'Задачи с указанным сроком' и добавить срок выполнения задачи,
-            если сообщение похоже на задачу для которой указан срок.
-            Ты должен отвечать на сообщение 'Задача без срока', если соощение похоже на задачу, но для нее не указан срок выполнения.
-            Ты должен ответить на вопрос, если сообщение похоже на вопрос.
-            Ты должен сделать то, что указано в сообщении, если сообщение похоже на просьбу\n"""
+prompts_base_sys = SystemMessage(
+    content=""" Ты ИИ-ассистент, который помогает строить план действий для решение задачи. 
+                             Если введенное сообщение похоже на запрос о создании плана, то напиши 'Да', иначе напиши 'Нет' """
+)
+
+prompts_base = """Ты ИИ-ассистент, который помогает строить план действий для решения задачи. 
+                             Если введенное сообщение похоже на запрос о создании плана, то напиши 'Да', иначе напиши 'Нет'"""
+
+prompts_for_roadmap = SystemMessage(
+    content="""Представь, что ты умный студент. Разбей изучение введенной темы на пункты\n
+    Напиши каждый пункт в отдельной строке"""
     # "Ты таск-менеджер, который получает информацию о сроках выполнения задачи, получает ее описание и дает ей название\n"
 )
 
 messages = {}
 
 
-def message_processing(user_id: int, message: str) -> str:
+def message_processing(tg_id: int, message: str) -> str:
 
-    error_message = "Произошла техническая ошибка, попробуйте снова"
-    quastion = "Дай ответ на следующее сообщение:\n" + message
-    # if user_id in messages:
-    #     if len(messages[user_id]) >= 10:
-    #         last_messages = messages[user_id][2:]
-    #         messages[user_id] = [prompts_base] + last_messages
-    #     messages[user_id].append(HumanMessage(content=quastion))
-    # else:
-    #     messages[user_id] = [prompts_base]
-    #     messages[user_id].append(HumanMessage(content=quastion))
-    # res = chat(messages[user_id]).content
-    res = chat([prompts_base, HumanMessage(content=quastion)]).content
+    quastion = prompts_base + "\n" + message
 
-    return res
+    try:
+        res = chat([HumanMessage(content=quastion)]).content
+
+        if res.lower() == "да":
+            plan = chat([prompts_for_roadmap, HumanMessage(content=message)]).content
+
+            list_tasks_for_db = processing_for_add_in_db(plan, message)
+
+            for el in list_tasks_for_db:
+                insert_user_task(tg_id, el)
+
+            return "Данные успешно добавлены"
+        else:
+            if tg_id in messages:
+                if len(messages[tg_id]) >= 10:
+                    last_messages = messages[tg_id][2:]
+                    messages[tg_id] = [prompts_base] + last_messages
+                    messages[tg_id].append(HumanMessage(content=quastion))
+            else:
+                messages[tg_id] = [prompts_base]
+                messages[tg_id].append(HumanMessage(content=quastion))
+            res = chat(messages[tg_id]).content
+            return res
+    except Exception as e:
+        logging.critical(f"Failed in message_processing: {e}", exc_info=True)
+        return "Произошла ошибка, попробуйте снова"
+
+
+def get_recomindation(user_id: int, message: str) -> str:
+    pass
